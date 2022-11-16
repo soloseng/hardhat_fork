1eb4e004069dd6df7f59b399df82bb0cfd3e4282
soloseng <102702451+soloseng@users.noreply.github.com>
2022-11-16 13:34:47 -0500

    using ethereumjs package

--- packages/hardhat-core/test/internal/util/packageInfo.ts
+++ packages/hardhat-core/test/internal/util/packageInfo.ts
@@ -10,7 +10,7 @@ import { getRealPath } from "../../../src/internal/util/fs-utils";
 describe("packageInfo", () => {
   it("Should give the right package.json", async () => {
     const packageJson = await getPackageJson();
-    assert.equal(packageJson.name, "hardhat");
+    assert.equal(packageJson.name, "@celo/hardhat");
     // We don't test the version number because that would be hard to maintain
     assert.isString(packageJson.version);
   });
